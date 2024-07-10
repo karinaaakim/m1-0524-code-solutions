b94ad83bc47d58d130b8c535bd95435f01dce378
Karina Kim <karinaaakim@gmail.com>
2024-07-10 02:27:46 +0000

    Completed typescript-and-json exercise.

---
+++ typescript-and-json/main.ts
@@ -0,0 +1,25 @@
+const array = [
+  {
+    isbn: '0-7289',
+    title: 'Kafka on the Shore',
+    author: 'Haruki Murakami',
+  },
+  {
+    isbn: '1-2674',
+    title: 'The Girl with the Dragon Tattoo',
+    author: 'Stieg Larsson',
+  },
+  {
+    isbn: '2-5297',
+    title: "On Earth We're Briefly Gorgeous",
+    author: 'Ocean Vuong',
+  },
+];
+
+const json = JSON.stringify(array);
+console.log(json, typeof json);
+
+const student = '{"number":"1738", "id":"505", "name":"Karina Kim"}';
+
+const studentParsed = JSON.parse(student);
+console.log(studentParsed, typeof studentParsed);
